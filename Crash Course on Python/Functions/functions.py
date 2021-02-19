bc850c92edd1be7ffc1cebfe7c33233b0a5382bc
Guy Friley <gfriley@hotmail.com>
2021-02-18 23:45:14 -0500

    Notes on Functions

---
+++ Crash Course on Python/Functions/functions.py
@@ -0,0 +1,18 @@
+'''
+Notes on Functions...
+
+'''
+
+# Defining a function
+
+# 1 Parameter
+def greeting(name):
+    print("Welcome, " + name)
+
+greeting("Pythonista")
+# 2 Parameters
+def greeting2(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+
+greeting2("Pythonista", "Engineering")
