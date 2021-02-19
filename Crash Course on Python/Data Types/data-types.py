c0836c454cc48aa8eeec85ac49aca9590e0b77e8
Guy Friley <gfriley@hotmail.com>
2021-02-18 22:43:45 -0500

    notes on data types

---
+++ Crash Course on Python/Data Types/data-types.py
@@ -0,0 +1,19 @@
+# Video Talks about Data Types
+
+my_string = "Hello, world!"
+print(my_string)
+
+my_int = 10
+print(my_int)
+
+my_float = 10.10
+print(my_float)
+
+my_bool = True
+print(my_bool)
+
+# Google is your friend, if you get stuck, use google for the error.
+
+#You can use the type() function to get the data type of a variable.
+
+print(type(my_string)) # prints data type to console.
