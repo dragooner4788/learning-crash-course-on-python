cc761e28ce55ddfc0633bc9d1715f74b5ea6a1f4
Guy Friley <gfriley@hotmail.com>
2021-02-18 23:18:31 -0500

    Notes on Expressions, Numbers, and Type Conversions

---
+++ Crash Course on Python/Variables/variables.py
@@ -0,0 +1,51 @@
+'''
+Learning About Variables...
+
+''' 
+
+# Variables: Names that we give to certain values in our programs.
+# Values can be of ANY data type.
+# Variables are just containers for data; computer holds data in memory for use later. 
+
+length = 10
+
+wdith = 2
+
+area = length * width
+
+print(area)
+
+# Assignment: The process of storing a value inside of a variable
+
+# Expression: A combination of number, symbols, or other variables that produce a result when evaluated
+
+# Variable = Value
+
+'''
+Variable Naming Restrictions...
+
+- Don't use keywords or functions that PYthon reserves for it's own use. 
+
+- Don't use spaces
+
+- Must start with a letter or underscore
+
+- Must be made up of only:
+
+    - Letters
+    - Numbers
+    - Underscores
+
+Valid Variables:
+
+i_am_a_variable --> Valid
+
+i_am_a_variable2 --> valid
+
+1_is_a_numver --> invalid # Cannot start with a number!
+
+apples_&_oranges --> invalid # Cannot use a special character!
+
+Variables are case sensitive...
+
+'''
